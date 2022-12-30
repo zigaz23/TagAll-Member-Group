06269a4c554a56f4998e177683ff609e4bc6c5e9
hdiiofficial <79190751+hdiiofficial@users.noreply.github.com>
2022-12-30 20:20:03 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -18,7 +18,10 @@ LOGGER = logging.getLogger(__name__)
 api_id = int(os.environ.get("APP_ID"))
 api_hash = os.environ.get("API_HASH")
 bot_token = os.environ.get("TOKEN")
+DB_URL = os.environ.get("DB_URL")
+ADMINS = int(os.environ.get("ADMINS"))
 ribot = TelegramClient('client', api_id, api_hash).start(bot_token=bot_token)
+
 
 moment_worker = []
 
